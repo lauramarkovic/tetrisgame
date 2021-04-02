7ef3897c5252a129b4d1b37b1668793d02c2327c
Laura Markovičová <itsmelararogers@MacBook-Air-uzivatela-Laura.local>
2021-04-02 19:21:54 +0200

    added useMemo()

--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -1,11 +1,12 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useMemo } from 'react';
 
 export const useGameStatus = rowsCleared => {
   const [score, setScore] = useState(0);
   const [rows, setRows] = useState(0);
   const [level, setLevel] = useState(0);
 
-  const linePoints = [40, 100, 300, 1200];
+  let linePoints = useMemo(() => { }, []);
+  linePoints = [40, 100, 300, 1200];
 
   const calcScore = useCallback(() => {
     // We have score
